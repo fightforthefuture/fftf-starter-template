925630e2da1222dd91d98f6892bab58c015f0c02
Meredith Laverty <lavertym@gmail.com>
2019-09-17 17:05:59 -0400

    add joint petition form component

---
+++ components/JointPetitionForm.vue
@@ -0,0 +1,474 @@
+<i18n src="~/locales/components/ActionNetworkForm.yml"></i18n>
+<i18n src="~/locales/global.yml"></i18n>
+
+<template>
+  <div class="sml-push-y2 med-push-y3">
+    <div class="text-center" v-if="hasSigned">
+      <div class="fill-grey-dark sml-pad-2 lrg-pad-3 is-rounded">
+        <h2 class="text-success">{{ $t('thanks.title') }}</h2>
+        <p class="sml-push-y1 text-success">{{ $t('thanks.share') }}</p>
+        <div class="row sml-push-y2 med-push-y3">
+          <div class="sml-c12 lrg-c4">
+            <ShareButton
+              network="twitter"
+              class="btn-block"
+              :text="tweetText"
+              @click.native="$trackClick(`twitter_share_button_success_${routeName}`)">
+              <span>{{ $t('global.common.tweet') }}</span>
+            </ShareButton>
+          </div> <!-- .c -->
+          <div class="sml-c12 lrg-c4 sml-push-y1 lrg-push-y0">
+            <ShareButton
+              network="facebook"
+              class="btn-block"
+              @click.native="$trackClick(`facebook_share_button_sucess_${routeName}`)">
+              <span>{{ $t('global.common.share') }}</span>
+            </ShareButton>
+          </div> <!-- .c -->
+          <div class="sml-c12 lrg-c4 sml-push-y1 lrg-push-y0">
+            <a :href="donateUrl"
+               class="btn btn-block btn-bright"
+               @click="$trackClick(`donate_button_success_${routeName}`)">
+              <span>{{ $t('global.common.donate') }}</span>
+            </a>
+          </div> <!-- .c -->
+        </div> <!-- .row -->
+      </div> <!-- v-if -->
+    </div> <!-- .push -->
+
+    <div v-else>
+      <div
+        :id="'can-petition-area-'+$t('petition_url_string')"
+        style="display: none">
+          <!-- The Action Network form embed will be inserted here -->
+      </div>
+
+      <form
+          @submit.prevent="submitForm()"
+          action="https://queue.fftf.xyz/action"
+          method="post"
+          >
+        <input type="hidden" name="subject" :value="subject">
+        <input type="hidden" name="hp_enabled" value="true">
+        <input type="hidden" name="guard" value="">
+        <input type="hidden" name="contact_congress" :value="shouldContactCongress">
+        <input type="hidden" name="fcc_ecfs_docket" :value="fccDocket">
+        <input type="hidden" name="an_tags" :value="mothershipTags">
+        <input type="hidden" name="an_petition_id" :value="anPetitionId">
+        <input type="hidden" name="redirect_to" :value="$t('redirect_url')">
+
+        <p v-if="errorMessage" class="text-warn">
+          {{ errorMessage }}
+        </p>
+        <div class="flex-grid sml-flex-row">
+          <input v-model="name"
+            :placeholder="$t('form.name')"
+            type="text"
+            name="member[first_name]"
+            required>
+          <input v-model="email"
+            :placeholder="$t('form.email')"
+            type="email"
+            name="member[email]"
+            required>
+        </div> <!-- .flex-grid -->
+        <div class="flex-grid sml-flex-row sml-push-y1">
+          <input v-model="address"
+                 type="text"
+                 class="sml-flex-2"
+                 :placeholder="`${$t('form.address')}${shouldContactCongress === 1 ? '*' : ''}`"
+                 name="member[street_address]"
+                 :required="contactCongress === 1">
+          <input v-model="zipCode"
+                 type="tel"
+                 :placeholder="$t('form.zip')"
+                 name="member[postcode]"
+                 required>
+          <input v-model.trim="phone"
+                 type="tel"
+                 class="sml-flex-2"
+                 :placeholder="$t('form.phone')"
+                 name="member[phone_number]">
+        </div> <!-- .flex-grid -->
+        <div v-if="hasCompany" class="sml-push-y1">
+          <div v-if="hasCompanyToggle"
+               class="flex-grid sml-flex-row flex-center sml-push-y1">
+            <p class="sml-flex-2 med-flex-3 text-left">
+              {{ $t('form.is_an_org') }}
+            </p>
+            <div class="radio-toggle sml-pad-half">
+              <div class="flex-grid sml-flex-row">
+                <div>
+                  <input
+                    v-model="isBusinessOwner"
+                    type="radio"
+                    :value="false"
+                    id="not-biz">
+                  <label for="not-biz">{{ $t('global.common.no') }}</label>
+                </div>
+                <div>
+                  <input
+                    v-model="isBusinessOwner"
+                    type="radio"
+                    :value="true"
+                    id="is-biz">
+                  <label for="is-biz">{{ $t('global.common.yes') }}</label>
+                </div>
+              </div> <!-- .flex-grid -->
+            </div> <!-- .radio-toggle -->
+          </div> <!-- .flex-grid -->
+
+          <div v-if="isBusinessOwner || !hasCompanyToggle" class="sml-push-y1">
+            <input
+              v-model="companyName"
+              type="text"
+              :placeholder="`${$t('form.company')}${hasCompanyToggle ? '*': ''}`"
+              :required="hasCompanyToggle"
+              name="member[company]">
+          </div> <!-- v-if isBusinessOwner -->
+        </div> <!-- v-if hasCompany -->
+        <div v-if="hasComment || shouldContactCongress"
+             class="sml-push-y1 textarea-with-btn">
+          <textarea
+            v-model="comment"
+            ref="comment"
+            :placeholder="$t('form.comment')"
+            name="action_comment"
+            required>
+          </textarea>
+          <a class="btn btn-sml btn-alt" @click.prevent="clearComment()">
+            {{ $t('global.common.clear') }}
+          </a>
+        </div> <!-- .textarea-with-btn -->
+
+        <button class="btn btn-block btn-cta sml-push-y1" :disabled="isSending">
+          <span v-if="isSending">{{ $t('global.common.sending') }}</span>
+          <span v-else>{{ buttonText }}</span>
+        </button>
+        <p class="sml-push-y1 text-center">
+          <small v-html="$t('join_petition_disclaimer_html')"></small>
+        </p>
+      </form>
+    </div> <!-- /!hasSigned -->
+
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import { sendToMothership } from '~/assets/js/helpers'
+import ShareButton from '~/components/ShareButton'
+
+function setValue(inputId, newValue) {
+  const input = document ? document.getElementById(inputId) : null
+
+  if (input) {
+    input.value = newValue
+  }
+}
+
+export default {
+  components: {
+    ShareButton
+  },
+
+  props: {
+    anPetitionId: {
+      type: String,
+      required: false,
+      default: function () {
+        return this.$t('petition_id')
+      }
+    },
+    subject: {
+      type: String,
+      required: false,
+      default: function () {
+        return this.$t('subject')
+      }
+    },
+    contactCongress: {
+      type: String,
+      required: false,
+      default: function () {
+        return this.$t('contact_congress')
+      }
+    },
+    /* eslint-disable vue/require-prop-types */
+    fccDocket: {
+      required: false,
+      default: function () {
+        return this.$te('fcc_docket') ? this.$t('fcc_docket') : null
+      }
+    },
+    callpowerId: {
+      required: false,
+      default: function () {
+        return this.$te('global.callpower_id') ? this.$t('global.callpower_id') : null
+      }
+    },
+    /* eslint-enable vue/require-prop-types */
+    tags: {
+      type: Object,
+      required: false,
+      default: function () {
+        return this.$t('tags')
+      }
+    },
+    textFlowId: {
+      type: String,
+      required: false,
+      default: function () {
+        return this.$t('text_flow_id')
+      }
+    },
+    buttonText: {
+      type: String,
+      required: false,
+      default: function () {
+        return this.$t('form.button_cta')
+      }
+    },
+    hasComment: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    hasCompany: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    hasCompanyToggle: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    tweetText: {
+      type: String,
+      required: false,
+      default: null
+    },
+    letterText: {
+      type: String,
+      required: false,
+      default: function () {
+        return this.$t('global.letter_text')
+      }
+    }
+  },
+
+  data() {
+    return {
+      isSending: false,
+      hasSigned: false,
+      errorMessage: null,
+      comment: null,
+      isBusinessOwner: false,
+      companyName: null,
+      referrerGroup: null
+    }
+  },
+
+  computed: {
+    ...mapState(['donateUrl']),
+
+    routeName() { return this.$nuxt.$route.name },
+
+    name: {
+      get() {
+        return this.$store.state.name
+      },
+      set(value) {
+        this.$store.commit('setName', value)
+      }
+    },
+
+    email: {
+      get() {
+        return this.$store.state.email
+      },
+      set(value) {
+        this.$store.commit('setEmail', value)
+      }
+    },
+
+    address: {
+      get() {
+        return this.$store.state.address
+      },
+      set(value) {
+        this.$store.commit('setAddress', value)
+      }
+    },
+
+    zipCode: {
+      get() {
+        return this.$store.state.zipCode
+      },
+      set(value) {
+        this.$store.commit('setZipCode', value)
+      }
+    },
+
+    phone: {
+      get() {
+        return this.$store.state.phone
+      },
+      set(value) {
+        this.$store.commit('setPhone', value)
+      }
+    },
+
+    shouldContactCongress() {
+      return this.contactCongress.toLowerCase() === 'yes' ? 1 : 0
+    },
+
+    mothershipTags() {
+      return JSON.stringify(Object.values(this.tags))
+    },
+
+    referrer() {
+      return this.$route.query.referrer
+    },
+
+    petitionString() {
+      return this.$t('petition_url_string')
+    },
+
+    firstName() {
+      const words = (this.name || '').split(' ')
+      return words.shift()
+    },
+
+    lastName() {
+      const words = (this.name || '').split(' ')
+      words.shift()
+      return words.join(' ')
+    }
+  },
+
+  watch: {
+    name(newValue) {
+      setValue('form-first_name', this.firstName)
+      setValue('form-last_name', this.lastName)
+    },
+
+    email(newValue) {
+      setValue('form-email', newValue)
+    },
+
+    address(newValue) {
+      setValue('form-street', newValue)
+    },
+
+    zipCode(newValue) {
+      setValue('form-zip_code', newValue)
+    },
+
+    comment(newValue) {
+      setValue('form-comments', newValue)
+    }
+  },
+
+  created() {
+    this.comment = this.letterText
+    this.hasSigned = this.hasSignedAlready
+  },
+
+  mounted() {
+    if (this.$route.query.signed) {
+      this.hasSigned = true
+      return
+    }
+
+    const script = document.createElement('script')
+    script.id = 'ACTION_NETWORK_EMBED_SCRIPT'
+    script.type = 'text/javascript'
+    script.async = true
+    script.src = `https://actionnetwork.org/widgets/v3/petition/${this.petitionString}?format=js&source=widget&clear_id=true&referrer=${this.referrer}`
+    document.body.appendChild(script)
+
+    document.addEventListener('can_embed_loaded', this.setupForm)
+    document.addEventListener('can_embed_submitted', this.handleFormSuccess)
+  },
+
+  destroyed() {
+    const script = document.getElementById('ACTION_NETWORK_EMBED_SCRIPT')
+
+    if (script) {
+      script.parentNode.removeChild(script)
+    }
+
+    document.removeEventListener('can_embed_loaded', this.setupForm)
+    document.removeEventListener('can_embed_submitted', this.handleFormSuccess)
+  },
+
+  methods: {
+    submitForm() {
+      if (!this.isSending) {
+        this.isSending = true
+        document.querySelector('#new_signature input[name="commit"]').click()
+      }
+    },
+
+    setupForm() {
+      // fill in AN form
+      setValue('form-first_name', this.firstName)
+      setValue('form-last_name', this.lastName)
+      setValue('form-street', this.address)
+      setValue('form-zip_code', this.zipCode)
+      setValue('form-comments', this.comment)
+
+      // Log referrer for debugging
+      const labels = document.querySelectorAll('#d_sharing label')
+      if (labels.length > 1) {
+        this.referrerGroup = labels[1].innerText.replace(/Opt in to updates from/, '').trim()
+        if (typeof console !== 'undefined') console.log(`Referring group: ${this.referrerGroup}`)
+      }
+    },
+
+    async handleFormSuccess() {
+      this.$trackEvent(`petition_swap_form_${this.routeName}`, 'submit')
+      this.$trackEvent(`petition_swap_form_${this.routeName}_${this.referrerGroup}`, 'submit')
+
+      if (this.shouldContactCongress) {
+        try {
+          await sendToMothership({ // eslint-disable-line no-unused-vars
+            subject: this.subject,
+            member: {
+              first_name: this.name,
+              email: this.email,
+              phone_number: this.phone,
+              street_address: this.address,
+              postcode: this.zipCode,
+              country: 'US'
+            },
+            hp_enabled: 'true',
+            guard: '',
+            contact_congress: true,
+            skip_action_network: true,
+            action_comment: this.comment
+          })
+        } catch (error) {
+          console.error(error)
+        }
+      }
+
+      if (this.callpowerId) {
+        this.$store.commit('setCallpowerCampaignId', this.callpowerId)
+        this.$store.commit('setModalVisibility', true)
+        this.$store.commit('setModalType', 'call-form')
+      }
+
+      this.isSending = false
+      this.hasSigned = true
+    },
+
+    clearComment() {
+      this.comment = ''
+      this.$refs.comment.focus()
+    }
+  }
+}
+</script>
