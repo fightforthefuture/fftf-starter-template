0c700267f05daa14b4ec5366b4dd19b30766f5be
Meredith Laverty <lavertym@gmail.com>
2020-01-14 10:27:51 -0500

    video header back into starter template

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,17 +1,34 @@
 <template>
-  <header class="sml-pad-y-top3 med-pad-y-top6">
-    <div class="wrapper">
-      <Logo class="grid-center text-center" />
-    </div> <!-- .wrapper -->
+  <header>
+    <VideoHeader v-if="video" />
+    <div class="sml-pad-y-top3 med-pad-y-top6">
+      <div class="wrapper">
+        <div class="row">
+          <div class="sml-c12 med-c10 grid-center text-center">
+            <Logo class="grid-center text-center sml-pad-y2" />
+          </div>
+        </div>
+      </div> <!-- .wrapper -->
+    </div>
   </header>
 </template>
 
 <script>
 import Logo from '~/components/Logo'
+import VideoHeader from '~/components/VideoHeader'
 
 export default {
   components: {
-    Logo
+    Logo,
+    VideoHeader
+  },
+
+  props: {
+    video: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
   }
 }
 </script>
