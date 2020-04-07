de1a58121beab9bc82c89dda23d627a6a7dd4434
Ken Mickles <ken@37i.net>
2020-04-07 16:04:43 -0400

    Migrate error page to Bootstrap

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,32 +1,19 @@
 <i18n src="~/locales/layouts/error.yml"></i18n>
 
 <template>
-  <DefaultLayout>
-    <section class="sml-pad-y3 med-pad-y6">
-      <div class="wrapper">
-        <div class="row">
-          <div class="sml-c12 lrg-c8 grid-center text-center">
-            <h1 v-if="error.statusCode === 404">{{ $t('404_title') }}</h1>
-            <h1 v-else>{{ error.statusCode }} - {{ error.message }}</h1>
-            <p class="sml-push-y2 med-push-y3">
-              {{ $t('return') }}
-              <nuxt-link :to="localePath('index')">{{ $t('homepage') }}</nuxt-link>.
-            </p>
-          </div> <!-- .c -->
-        </div> <!-- .row -->
-      </div> <!-- .wrapper -->
-    </section>
-  </DefaultLayout>
+  <div class="jumbotron jumbotron-fluid text-center">
+    <h1 class="display-4">
+      <span v-if="error.statusCode === 404">{{ $t('404_title') }}</span>
+      <span v-else>{{ error.statusCode }} - {{ error.message }}</span>
+    </h1>
+    <p class="lead">
+      <nuxt-link :to="localePath('index')">{{ $t('return_home') }}</nuxt-link>
+    </p>
+  </div>
 </template>
 
 <script>
-import DefaultLayout from '~/components/DefaultLayout'
-
 export default {
-  components: {
-    DefaultLayout
-  },
-
   props: {
     error: {
       type: Object,
