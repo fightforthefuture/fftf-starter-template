f8ea24130a089ed3b36e72955bb97f2506f1220f
Meredith Laverty <lavertym@gmail.com>
2020-05-07 16:29:43 -0400

    add video header component

---
+++ components/VideoHeader.vue
@@ -0,0 +1,27 @@
+<style lang="scss" scoped>
+  video.video-hero {
+    margin: 0 auto;
+    width: 100%;
+  }
+  .video-wrapper {
+    position: relative;
+  }
+  .video-wrapper:after {
+    background: linear-gradient(rgba($body-bg, 0), rgba($body-bg,1));
+    bottom:0;
+    content: '';
+    height:10vw;
+    left: 0;
+    position: absolute;
+    width:100%;
+  }
+</style>
+
+<template>
+  <div class="video-wrapper">
+    <video autoplay loop muted playsinline class="video-hero"
+           poster="images/video-bg-poster.jpg">
+      <source src="~assets/videos/video-header.mp4" type="video/mp4" />
+    </video>
+  </div>
+</template>
