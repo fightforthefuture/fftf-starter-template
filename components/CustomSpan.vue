af1b62d25dccd8ab5887bbd0920409898244d2af
Meredith Laverty <lavertym@gmail.com>
2021-03-03 14:30:24 -0500

    better space management on custom span

--- components/CustomSpan.vue
+++ components/CustomSpan.vue
@@ -1,3 +1,21 @@
 <template>
-  <span>&nbsp;<slot />&nbsp;</span>
+  <span><template v-if="!ltrim">&nbsp;</template><slot /><template v-if="!rtrim">&nbsp;</template></span>
 </template>
+
+<script>
+
+export default {
+  props: {
+    ltrim: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    rtrim: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  }
+}
+</script>
