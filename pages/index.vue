c673f72b1b58aae67b513bf5c3b96b6a9cf99533
Meredith Laverty <lavertym@gmail.com>
2021-10-21 21:36:27 -0400

    basics using cms.yml

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,23 @@
+<i18n src="~/locales/cms.yml"></i18n>
+
 <template lang="pug">
   main.index-page.text-center
-    include ../templates/index.html
+
+    section#intro(aria-labelledby="h1-intro")
+      h1#h1-intro(v-html="$t('cms.intro_title')")
+      .container
+        p(v-html="$t('cms.intro_subheading')")
+        a.btn.btn-link(@click.prevent="$scrollTo('#sign')")
+          img(src="~assets/images/arrow-down-light.svg" alt="Scroll down to sign the petition")
+
+    section#petition(aria-labelledby="h2-petition")
+      .container
+        h2#h2-petition(v-html="$t('cms.petition_title')")
+        p(v-html="$t('cms.petition_description')")
+        ActionNetworkForm(
+          :petition-id="$t('cms.an_petition_id')"
+          tags="$te('cms.petition_tags') ? $t('cms.petition_tags') : ''"
+        )
 </template>
 
 <script>
