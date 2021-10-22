bcc95b28eb436119909336cda14dc6ebeaacc27d
Meredith Laverty <lavertym@gmail.com>
2021-10-21 22:29:23 -0400

    remove component plugin

--- plugins/components.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import CustomA from '~/components/CustomA.vue'
-import CustomSpan from '~/components/CustomSpan.vue'
-import Uppercase from '~/components/Uppercase.vue'
-const components = { CustomA, CustomSpan, Uppercase }
-
-Object.entries(components).forEach(([name, component]) => {
-    Vue.component(name, component)
-})
